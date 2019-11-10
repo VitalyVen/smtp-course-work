ab62e23de38ed36a791a4d090af93222f791daf0
Vitaly <vitalyven@gmail.com>
2019-11-10 14:34:00 +0300

    docs:

--- deps-install.sh
+++ deps-install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 #cunit
-sudo apt-get install libcunit1 libcunit1-doc libcunit1-dev
+sudo apt-get install libcunit1 libcunit1-doc libcunit1-dev build-essential
 #clang
 sudo apt-get install clang gcc make evolution
+sudo apt-get install build-essential cflow doxygen valgrind dot2tex autogen check graphviz \
+texlive texlive-lang-cyrillic texlive-latex-base
