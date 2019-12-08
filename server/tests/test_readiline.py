bc7d91e1110cd03cbf238b81444a859f7adc39aa
sbn <zgreen3@gmail.com>
2019-12-08 18:10:20 +0300

    feat readline

--- server/tests/test_readiline.py
+++ server/tests/test_readiline.py
@@ -1,5 +1,20 @@
 import re
 import pytest
+import socket
+import * from main
 def test_readline():
     #launch me with pytest from root dir
+
+    # server = MailServer()
+    # TODO: make server.start()
+
+    # socket = socket.socket()
+    # with mock.patch('socket.socket') as mock_socket:
+    #     mock_socet.return_value.recv.return_vslue =
+    # address = 'localhost'
+    # port = 2556
+    # try:
+    #     socket.connect((address, port))
+
+
     assert False
