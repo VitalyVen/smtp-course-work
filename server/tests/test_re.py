b1b74e28219810b5ab1a656c114d962d57c44a0a
Vitaly <vitalyven@gmail.com>
2020-01-07 19:20:39 +0300

    tests: update hello pattern matching

--- server/tests/test_re.py
+++ server/tests/test_re.py
@@ -2,7 +2,8 @@ import re
 import pytest
 from server.state import  HELO_pattern, MAIL_FROM_pattern, DATA_start_pattern, DATA_end_pattern, RCPT_TO_pattern
 def test_hello_pattern():
-    assert re.match(HELO_pattern, 'HELO jlkjl@coml.com \r\n').group(1)=='jlkjl@coml.com'
+    assert re.match(HELO_pattern, 'HELO coml.com\r\n').group(1)=='HELO'
+    assert re.match(HELO_pattern, 'HELO coml.com\r\n').group(2)=='coml.com'
 def test_email_pattern():
     assert re.match(MAIL_FROM_pattern, 'MAIL FROM: <jlkjl@coml.com>\r\n').group(1)=='jlkjl@coml.com'
 def test_email_patternf():
