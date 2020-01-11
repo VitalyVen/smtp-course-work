566c32d2c2b9a0a0ed76f42ae3efca257f1bc089
Vitaly <vitalyven@gmail.com>
2020-01-11 13:16:29 +0300

    docs: report makefile changed as well

--- report/vitaly/make.py
+++ report/vitaly/make.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     # path = os.path.join(os.path.dirname(__file__), os.pardir, 'data/my_state_diagram_server.png')
     # draw_server_fsm(path)
 
+    os.system('make clean')
     os.system('make report-server.pdf')
     pdfs = ['Первые_2_страницы_v.pdf', 'report-server.pdf']
     merger = PdfFileMerger()
