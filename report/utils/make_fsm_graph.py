bd7fab0096be1e2c445f65c09ab693937533f211
Vitaly <vitalyven@gmail.com>
2020-01-11 11:40:23 +0300

    docs: report is started feat: utils for report

--- report/utils/make_fsm_graph.py
+++ report/utils/make_fsm_graph.py
@@ -2,10 +2,16 @@ import sys
 import os
 from server.SMTP_FSM import SMTP_FSM
 # from client.C import SMTP_FSM_CLIENT
-if __name__ == '__main__':
 
+def draw_server_fsm(abs_path):
     machine = SMTP_FSM('graph', '.')
-    machine.get_graph().draw(os.path.join(os.path.dirname(__file__), os.pardir, 'data/my_state_diagram_server.png'), prog='dot')
+    machine.get_graph().draw(abs_path,
+                             prog='dot')
+
+if __name__ == '__main__':
+    path = os.path.join(os.path.dirname(__file__), os.pardir, 'data/my_state_diagram_server.png')
+    draw_server_fsm(path)
+
 
     # machine = SMTP_FSM_CLIENT('graph')
     # machine.get_graph().draw(os.path.join(os.path.dirname(__file__), os.pardir, 'data/my_state_diagram_client.png'), prog='dot')
